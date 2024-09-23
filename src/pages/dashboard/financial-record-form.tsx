d72502d3a08631975d6bfede34d8f3dca89f787d
Jason Alvarado <jayxalvarado@gmail.com>
2024-09-23 10:18:23 -0700

    feat: Add financial record form and list components to dashboard

---
+++ src/pages/dashboard/financial-record-form.tsx
@@ -0,0 +1,3 @@
+export const FinancialRecordForm = () => {
+  return <div>Record Form</div>;
+};
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,15 @@
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinancialRecordList } from "./financial-record-list";
+
 export const Dashboard = () => {
+  const { user } = useUser();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="dashboard-container">
+      <h1>Welcome {user?.firstName}! Here Are Your Finance Records:</h1>
+      <FinancialRecordForm />
+      <FinancialRecordList />
     </div>
   );
 };
