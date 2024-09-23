3b255b548f14cde45c5c1271844cad21857fb593
Jason Alvarado <jayxalvarado@gmail.com>
2024-09-23 00:08:32 -0700

    feat: Add routing and pages for authentication and contact
    This commit adds routing and pages for authentication and contact. It introduces a new file, "auth/index.tsx", which contains the code for the authentication page. It also adds a new file, "contact/index.tsx", which contains the code for the contact page. Additionally, it creates a new file, "dashboard/index.tsx", which contains the code for the dashboard page. The App component in "App.tsx" is updated to include the new routes for these pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+import { Contact } from "./pages/contact";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/auth" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
